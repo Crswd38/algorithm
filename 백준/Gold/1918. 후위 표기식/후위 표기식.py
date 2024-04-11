a026b7d042e1859d132da52193d0b8b946b6c2f4
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-04-11 15:24:16 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,26 @@
+import sys
+
+strn = list(sys.stdin.readline())
+stack=[]
+res=''
+for s in strn:
+    if s.isalpha():
+        res+=s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] =='/'):
+                res += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                res+= stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+while stack :
+    res+=stack.pop()
+print(res)
