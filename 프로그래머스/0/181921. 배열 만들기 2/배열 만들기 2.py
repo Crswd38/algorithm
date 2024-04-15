dc8a7f873f47cb7e83416850b2f41ad7c4eb3338
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-04-15 16:32:19 +0900

    [level 0] Title: 배열 만들기 2, Time: 29.33 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,15 +1,7 @@
 def solution(l, r):
     result = []
     for i in range(l, r+1):
-        if "1" not in str(i):
-            if "2" not in str(i):
-                if "3" not in str(i):
-                    if "4" not in str(i):
-                        if "6" not in str(i):
-                            if "7" not in str(i):
-                                if "8" not in str(i):
-                                    if "9" not in str(i):
-                                        result.append(i)
-    if not result:
-        result.append(-1)
-    return result
+        if not set(str(i)) - set(['0', '5']):
+            result.append(i)
+            
+    return result if result else [-1]
