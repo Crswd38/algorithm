6b0fca6bbde2da5c2b7db5a86698e662b60de94c
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-04-15 16:52:30 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,5 +1,2 @@
 def solution(start_num, end_num):
-    answer = []
-    for i in range(start_num, end_num+1):
-        answer.append(i)
-    return answer 
+    return [i for i in range(start_num,end_num+1)]
