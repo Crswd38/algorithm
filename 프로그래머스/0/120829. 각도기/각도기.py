6b578194180e97f7a6b2255c983e1123f9062a49
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-04-16 13:07:27 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,2 @@
 def solution(angle):
-    if 0 < angle and angle < 90:
-        return 1
-    elif angle == 90:
-        return 2
-    elif angle < 180:
-        return 3
-    elif angle == 180:
-        return 4
+    return (angle // 90) * 2 + (angle % 90 > 0)
