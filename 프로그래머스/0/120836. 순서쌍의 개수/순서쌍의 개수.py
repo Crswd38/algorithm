407d98ce54ab82e1347c251ecc4343a6bbe22548
Crswd38 <sinsoulward38@gmail.com>
2024-04-19 17:23:46 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,8 @@
 def solution(n):
-    result = []
-    for i in range(1, n+1):
-        if n%i == 0:
-            result.append(i)
-    return len(result)
+    answer = 0
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            answer += 2
+            if i * i == n:
+                answer -= 1
+    return answer
