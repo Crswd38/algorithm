72f869fda3d0f92851d4f50a53e3fe6659491534
Crswd38 <sinsoulward38@gmail.com>
2024-04-22 10:24:09 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,19 @@
+def solution(rsp):
+    result = ""
+    for i in rsp:
+        if i == "0":
+            result += "5"
+        elif i == "2":
+            result += "0"
+        else:
+            result += "2"
+    return result
+    
+    
+    
+    
+    
+    
+    
+    
+    return answer
