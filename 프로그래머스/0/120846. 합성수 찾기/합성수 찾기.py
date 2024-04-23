01c51d838253b946a85819c25112b704521bae38
Crswd38 <sinsoulward38@gmail.com>
2024-04-23 15:22:16 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,14 @@
+def sieve_of_eratosthenes(n):
+    prime = [True for i in range(n+1)]
+    p = 2
+    while p**2 <= n:
+        if prime[p] == True:
+            for i in range(p**2, n+1, p):
+                prime[i] = False
+        p += 1
+    prime[0] = False
+    prime[1] = False
+    return prime
+
+def solution(n):
+    return sieve_of_eratosthenes(n).count(False)-2
