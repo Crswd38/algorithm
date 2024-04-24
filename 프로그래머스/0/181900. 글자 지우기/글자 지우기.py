76e4d552c3a31ed4da804a35f8c20aeb540595eb
Crswd38 <sinsoulward38@gmail.com>
2024-04-24 11:46:19 +0900

    [level 0] Title: 글자 지우기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,9 +1,6 @@
 def solution(my_string, indices):
-    result = []
-    my_string = list(my_string)
-    for i in range(len(indices)):
-        for j in range(i, len(indices)):
-            if indices[j] > indices[i]:
-                indices[j] -= 1
-        my_string.pop(indices[i])
-    return ''.join(my_string)
+    answer = ''
+    for i in range(len(my_string)):
+        if i not in indices:
+            answer += my_string[i]
+    return answer
