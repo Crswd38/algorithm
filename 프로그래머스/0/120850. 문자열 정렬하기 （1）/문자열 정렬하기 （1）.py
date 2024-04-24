afae603169a30cacefb550570d9e3dd3d5360871
Crswd38 <sinsoulward38@gmail.com>
2024-04-24 09:28:26 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,7 +1,2 @@
 def solution(my_string):
-    result = []
-    for i in my_string:
-        if i.isnumeric():
-            result.append(int(i))
-    result.sort()
-    return result
+    return sorted([int(c) for c in my_string if c.isdigit()])
