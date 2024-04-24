db1564641caac210d8942b1e605180d6ae28f27b
Crswd38 <sinsoulward38@gmail.com>
2024-04-24 09:16:30 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/README.md
+++ 프로그래머스/0/120849. 모음 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모음 제거 - 120849 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.05 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 09:15:24
+2024년 04월 24일 09:16:28
 
 ### 문제 설명
 
--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,14 +1,2 @@
 def solution(my_string):
-    my_string = list(my_string)
-    for _ in range(len(my_string)):
-        if "a" in my_string:
-            my_string.remove("a")
-        elif "e" in my_string:
-            my_string.remove("e")
-        elif "i" in my_string:
-            my_string.remove("i")
-        elif "o" in my_string:
-            my_string.remove("o")
-        elif "u" in my_string:
-            my_string.remove("u")
-    return ''.join(my_string)
+    return "".join([i for i in my_string if not(i in "aeiou")])
