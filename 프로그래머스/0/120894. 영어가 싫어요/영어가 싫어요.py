e3f62616b423527720c03e9b417da5dc61a4dd13
Crswd38 <sinsoulward38@gmail.com>
2024-04-25 13:23:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,34 +1,4 @@
 def solution(numbers):
-    result = []
-    while(len(numbers) != 0):
-        if numbers[0:4] == "zero":
-            result.append("0")
-            numbers = numbers[4:]
-        elif numbers[0:3] == "one":
-            result.append("1")
-            numbers = numbers[3:]
-        elif numbers[0:3] == "two":
-            result.append("2")
-            numbers = numbers[3:]
-        elif numbers[0:5] == "three":
-            result.append("3")
-            numbers = numbers[5:]
-        elif numbers[0:4] == "four":
-            result.append("4")
-            numbers = numbers[4:]
-        elif numbers[0:4] == "five":
-            result.append("5")
-            numbers = numbers[4:]
-        elif numbers[0:3] == "six":
-            result.append("6")
-            numbers = numbers[3:]
-        elif numbers[0:5] == "seven":
-            result.append("7")
-            numbers = numbers[5:]
-        elif numbers[0:5] == "eight":
-            result.append("8")
-            numbers = numbers[5:]
-        elif numbers[0:4] == "nine":
-            result.append("9")
-            numbers = numbers[4:]
-    return int(''.join(result))
+    for num, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(num))
+    return int(numbers)
