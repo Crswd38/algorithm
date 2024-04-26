d19784067f23bb43b317dc9114ba593fcd9da809
Crswd38 <sinsoulward38@gmail.com>
2024-04-26 10:20:04 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,6 +1,2 @@
 def solution(n, numlist):
-    result = []
-    for i in numlist:
-        if i % n == 0:
-            result.append(i)
-    return result
+    return [i for i in numlist if i % n == 0]
