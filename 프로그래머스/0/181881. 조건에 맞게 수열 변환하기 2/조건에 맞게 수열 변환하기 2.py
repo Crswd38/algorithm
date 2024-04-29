36ed2da5196a15711d4c9f419f3c78ba57c04c54
Crswd38 <sinsoulward38@gmail.com>
2024-04-29 16:35:25 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    x = 0
+    while(True):
+        next = []
+        for i in arr:
+            if i >= 50 and i % 2 == 0 :
+                next.append(i // 2)
+            elif i < 50 and i % 2 == 1:
+                next.append(i*2 + 1)
+            else:
+                next.append(i)
+        if arr == next:
+            return x
+        else:
+            x += 1
+            arr = next
