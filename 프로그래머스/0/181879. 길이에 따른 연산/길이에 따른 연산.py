490b53855fcb9a45469a4ec278e6e9ad20c0165b
Crswd38 <sinsoulward38@gmail.com>
2024-04-29 17:03:19 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,4 +1,3 @@
-from functools import reduce
-
+from math import prod
 def solution(num_list):
-    return sum(num_list) if len(num_list) >= 11 else reduce(lambda x, y: x*y, num_list) 
+    return sum(num_list) if len(num_list) >= 11 else prod(num_list)
