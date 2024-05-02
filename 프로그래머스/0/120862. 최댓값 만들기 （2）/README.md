bdd95f6e7cffec9997f3c036f10416391723dc66
Crswd38 <sinsoulward38@gmail.com>
2024-05-02 11:03:53 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.1 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 02일 11:02:11
+2024년 05월 02일 11:03:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,10 +1,4 @@
 def solution(numbers):
     numbers.sort()
-    if numbers[0] < 0 and numbers[1] < 0:
-        if numbers[len(numbers)-1] * numbers[len(numbers)-2] > numbers[0] * numbers[1]:
-            return numbers[len(numbers)-1] * numbers[len(numbers)-2]
-        else:
-            return numbers[0] * numbers[1]
-    else:
-        return numbers[len(numbers)-1] * numbers[len(numbers)-2]
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2]) 
     
