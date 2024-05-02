8b28885029b73588fa4221f41649d97316a9fe0b
Crswd38 <sinsoulward38@gmail.com>
2024-05-02 15:20:48 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,2 +1,2 @@
 def solution(myString, pat):
-    return 1 if pat in ''.join(['B' if i == 'A' else 'A' for i in myString]) else 0
+    return int(''.join(['B' if i == 'A' else 'A' for i in pat]) in myString)
