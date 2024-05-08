9361a6e1c757f083da4ab223bd3cdf8c41945821
Crswd38 <sinsoulward38@gmail.com>
2024-05-08 13:10:20 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,16 +1,9 @@
 from math import gcd
+
 def solution(a, b):
-    arr = []
-    i = 2
-    if gcd(a, b) != 1:
-        a, b = a//gcd(a, b), b//gcd(a, b)
-    while b >= i:
-        if b % i == 0:
-            arr.append(i)
-            b /= i
-        else:
-            i += 1
-    return 1 if len(arr) == arr.count(2) + arr.count(5) else 2
-        
-    
-    
+    b //= gcd(a,b)
+    while b % 2 == 0:
+        b //= 2
+    while b % 5 == 0:
+        b //= 5
+    return 1 if b == 1 else 2
