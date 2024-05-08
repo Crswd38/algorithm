470d4adfdd1ed9528365b0c9183afbd8a7a7a5d5
Crswd38 <sinsoulward38@gmail.com>
2024-05-08 17:40:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,2 +1,2 @@
 def solution(left, right):
-    return sum(i if len([1 for j in range(1, i+1) if i%j==0])%2 == 0 else -i for i in range(left, right+1))
+    return sum(i if (i ** 0.5) % 1 else -i for i in range(left, right+1))
