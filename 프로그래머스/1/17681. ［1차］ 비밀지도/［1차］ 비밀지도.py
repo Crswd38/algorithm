7b3df0728f5538e93c5db607ae6a3d8ad5796b38
Crswd38 <sinsoulward38@gmail.com>
2024-05-30 17:32:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,15 +1,5 @@
+
 def solution(n, arr1, arr2):
-    arr = ["" for _ in range(n)]
-    str = ""
-    for i in range(n):
-        arr1[i] = bin(arr1[i])[2:].rjust(n, "0")
-        arr2[i] = bin(arr2[i])[2:].rjust(n, "0")
-    for i in range(n):
-        for j in range(n):
-            if int(arr1[i][j]) or int(arr2[i][j]):
-                str += "#"
-            else:
-                str += " "
-        arr[i] = str
-        str = ""
-    return arr
+    return [str(bin(i|j)[2:]).rjust(n,'0').replace('1','#').replace('0',' ') for i,j in zip(arr1,arr2)]
+    
+        
