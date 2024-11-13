ae013a8125cae6256657d902f88b489065458196
Crswd38 <sinsoulward38@gmail.com>
2024-11-13 14:24:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 624.64 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,19 +1,17 @@
+from collections import deque
+
+def check(s):
+    while True:
+        if "()" in s: s=s.replace("()","")
+        elif "{}" in s: s=s.replace("{}","")
+        elif "[]" in s: s=s.replace("[]","")
+        else: return False if s else True       
+
 def solution(s):
-    answer = 0
-    circle = s*2
+    ans = 0
+    que = deque(s)
 
     for i in range(len(s)):
-        sub = circle[i:i+len(s)]
-        stack = []
-        for j in sub:
-            if not stack :
-                stack.append(j)
-                continue
-            if ((stack[-1]=='['and j==']') or
-                (stack[-1]=='('and j==')') or
-                (stack[-1]=='{'and j=='}')):
-                stack.pop()
-            else:
-                stack.append(j)
-        answer += 1 if not stack else 0
-    return answer
+        if check(''.join(que)): ans+=1
+        que.rotate(-1)
+    return ans
