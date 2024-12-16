a376a177df67a5c574b14fd7c867f0222f5f24ef
Crswd38 <sinsoulward38@gmail.com>
2024-12-16 17:33:28 +0900

    [level 2] Title: 전화번호 목록, Time: 484.63 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,12 @@
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(1, len(phone_book)):
-        if phone_book[i-1] == phone_book[i][:len(phone_book[i-1])]:
-            return False
-    return True
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
